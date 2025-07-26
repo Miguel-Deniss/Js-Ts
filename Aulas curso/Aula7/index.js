df89953166e473f555a3327f4ee8871037dff2f9
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-07-26 19:14:17 -0300

    Create index.js

---
+++ Aulas curso/Aula7/index.js
@@ -0,0 +1,11 @@
+/*
+Não podemos criar constantes com palavras reservadas
+Constantes precisam ter nomes significativos
+Não pode começar o nome de uma constante com um número
+Não podem conter espaços ou traços
+Utilizamos camlCase
+Case-sensitive
+Não poe modigicar o valor de um constante
+Não utilizar VAR, utilizar CONST.
+*/
+const
