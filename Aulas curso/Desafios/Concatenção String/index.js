3608f264c2306885bde194e548bff49807ea201f
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-07-26 22:56:55 -0300

    Resolvendo desafios

---
+++ Aulas curso/Desafios/Concatenção String/index.js
@@ -0,0 +1,5 @@
+const nome = "Miguel";
+const idade = 16;
+const cidade = "Brotas";
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`)
---
+++ Aulas curso/Desafios/Temperatura/index.js
@@ -0,0 +1,8 @@
+let f = 200;
+let c = 200;
+
+fConta = c * (9 / 5) + 32;
+console.log("Fahrenheit =",fConta);
+
+cConta = (f - 32) * 5 / 9;
+console.log("Celsius =",cConta)
