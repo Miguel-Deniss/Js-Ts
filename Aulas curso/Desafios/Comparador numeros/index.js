4dcc79776c1456ae582b2cc87a6630ab12207000
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-07-27 00:02:47 -0300

    Update index.js

--- Aulas curso/Desafios/Comparador numeros/index.js
+++ Aulas curso/Desafios/Comparador numeros/index.js
@@ -1,10 +1,18 @@
-let num1 = 3;
+/*
+ERRADO - Consertar depois
+
+let num1 = 4;
 let num2 = 4;
 let verif;
 
 
 if(verif = num1 >= num2){
-  console.log(`${num1} maior`)
-} else {
-  return 'menor';
-}
+  console.log(`Maior`)
+} 
+else if (verif = num1 <= num2) {
+  console.log(`Menor`);
+} 
+else ( num1 = num2) 
+  console.log('Iguais');
+
+*/
