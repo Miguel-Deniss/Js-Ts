e159de769eca277902677c71f340f3582f489e03
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-07-27 00:21:54 -0300

    Create index.js

---
+++ Aulas curso/Desafios/Calculadora/index.js
@@ -0,0 +1,15 @@
+//Calculadora simples
+
+let num1;
+let num2;
+let resultado; 
+
+num1 = 20;
+num2 = 10;
+
+const soma = num1 + num2;
+const subt = num1 - num2;
+const mult = num1 * num2;
+const div = num1 / num2;
+
+console.log() // Escolher qual operador quer usar
