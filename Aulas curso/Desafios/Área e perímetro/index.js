43e3ef8404b0e82c5efca069bd69ee41836b6d38
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-07-28 01:29:10 -0300

    .

--- Aulas curso/Desafios/Área e perímetro/index.js
+++ Aulas curso/Desafios/Área e perímetro/index.js
@@ -6,7 +6,7 @@ num1 = 4
 num2 = 5
 
 area = num1 * num2;
-perimetro = num1 + num2 + num1 + num2;
+perimetro = num1 * 2 + num2 * 2;
 
 console.log(`O valor da área é: ${area}`);
 console.log(`O valor do perímetro é: ${perimetro}`)
