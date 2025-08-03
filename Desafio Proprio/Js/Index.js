2225df6a645f484d286d612942c925164e8c8335
Miguel-Deniss <baiozin.denisovas@gmail.com>
2025-08-03 15:39:34 -0300

    Desafio

--- Desafio Proprio/Js/Index.js
+++ Desafio Proprio/Js/Index.js
@@ -1,12 +1,25 @@
-// Desafio Proprio
-//Usuario 
+// Passo 1 e 2
+let nomeUsuario = prompt("Qual é o seu nome?");
+let idadeUsuario = parseInt(prompt("Qual é a sua idade?"));
 
-let nomeUsuario;
-let idadeUsuario;
+// Passo 3 e 4
+let nomeAmigo = prompt("Qual é o nome do seu amigo?");
+let idadeAmigo = parseInt(prompt("Qual é a idade do seu amigo?"));
 
+// Passo 5
+let querComparar = confirm("Você quer ver quem é mais velho?");
 
-//Amigo
-let nomeAmigo;
-let idadeAmigo;
+// Passo 6
+if (querComparar) {
+  if (idadeUsuario > idadeAmigo) {
+    alert(nomeUsuario + " é mais velho que " + nomeAmigo + ".");
+  } else if (idadeUsuario < idadeAmigo) {
+    alert(nomeAmigo + " é mais velho que " + nomeUsuario + ".");
+  } else {
+    alert(nomeUsuario + " e " + nomeAmigo + " têm a mesma idade.");
+  }
+}
 
-console.log(prompt());
+// Passo 7
+let somaIdades = idadeUsuario + idadeAmigo;
+alert("A soma das idades é: " + somaIdades);
