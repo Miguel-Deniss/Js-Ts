969921bd3d6f11e5d710efb03e3a003adb6f68ec
Miguel Denisovas <baiozin.denisovas@gmail.com>
2026-03-19 21:32:49 -0300

    Novo exercicio

--- Desafios/Calculo tarifas/index.js
+++ Desafios/Calculo tarifas/index.js
@@ -1,8 +1,20 @@
-//TENTAR DEPOIS
+const valorBasico = 2.5;
 
+function valorIdade() {
+  let idade = Number(document.getElementById("idade").value);
+  let tipoUsuario = document.getElementById("tipoUsuario").value;
+  let resultado = document.getElementById("resultado");
 
-
-const valorIncial = 2.50;
-
-function idade(age);
-if (age)
+  if (idade < 6) {
+    resultado.textContent = "Você deve pagar: R$ 0,00";
+  }
+  else if (idade >= 60) {
+    resultado.textContent = "Você deve pagar: R$ " + (valorBasico - (valorBasico * 30 / 100)).toFixed(2);
+  }
+  else if (tipoUsuario === "estudante") {
+    resultado.textContent = "Você deve pagar: R$ " + (valorBasico - (valorBasico * 50 / 100)).toFixed(2);
+  }
+  else {
+    resultado.textContent = "Você deve pagar: R$ " + valorBasico.toFixed(2);
+  }
+}
